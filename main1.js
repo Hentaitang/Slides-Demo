6c9dbf7829596152e0d0f750677b6748240bc35d
Hentaitang <mz374427166@gmail.com>
2018-07-25 20:45:06 +0800

    第二次增加样式

--- main1.js
+++ main1.js
@@ -4,20 +4,20 @@ var allButtons = $('.window > .buttons > span')
 var allImages = $('.window > .images > img')
 for(let i=0; i<allButtons.length+1; i++){
     $(`.buttons > span:nth-child(${i})`).on('click',function(){
+        let index = $(`.buttons > span:nth-child(${i})`).index()
         $('.images > img.current').addClass('leave').removeClass('current enter')
         .one('transitionend', ()=>{
             $(`.images > img.leave`).addClass('enter').removeClass('leave current')
             })
         $(`.images > img:nth-child(${i})`).addClass('current').removeClass('enter leave')
+        n = index + 1
     })
 }
 
+n = 0
 var timer = setInterval(()=>{
-    Leave(n).one('transitionend', ()=>{
-        Enter(n)
-    })
-    Current(n)
     n += 1
+    $(`.buttons > span:nth-child(${x(n)})`).trigger('click')
 }, 4000)
 
 $('.window').on('mouseenter', function(){
@@ -26,18 +26,10 @@ $('.window').on('mouseenter', function(){
 
 $('.window').on('mouseleave', function(){
     timer = setInterval(()=>{
-        Leave(n)
-        .one('transitionend', ()=>{
-            Enter(n)
-        })
-        Current(n)
         n += 1
+        $(`.buttons > span:nth-child(${x(n)})`).trigger('click')
     }, 4000)
 })
-
-
-
-
 
 // 下面不用看
 function x(n){
@@ -54,16 +46,4 @@ function initialization(){
     n = 1
     $(`.images > img:nth-child(${n})`).addClass('current')
     .siblings().addClass('enter')
-}
-
-function Current(n){
-    return $(`.images > img:nth-child(${x(n+1)})`).addClass('current').removeClass('enter leave')
-}
-
-function Leave(n){
-    return $(`.images > img:nth-child(${x(n)})`).addClass('leave').removeClass('current enter')
-}
-
-function Enter(n){
-    return $(`.images > img:nth-child(${x(n-1)})`).addClass('enter').removeClass('leave current')
 }
